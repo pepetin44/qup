d1d576fcbcd435372594a7c07ef24f9ed3d7a126
fraleiz17 <fraleiz17@gmail.com>
2014-07-13 10:24:07 -0500

    UnitVersion
    1.0

---
+++ system/language/spanish/error_lang.php
@@ -0,0 +1,59 @@
+<?php
+// CONTENT
+$lang['userNotAutorized']	    = "Lo sentimos, tu usuario no está autorizado para acceder a esta sección";
+$lang['accountRequired']	    = "Necesitas una cuenta para poder acceder a esta área. Regístrate, es gratis.";
+
+// EMAIL
+$lang['emailAlreadyUsed']	    = "Lo sentimos, ese correo ya está en uso.";
+$lang['userAlreadyUsed']	    = "Lo sentimos, ese usuario ya está en uso.";
+$lang['emailSentOk']		    = "Correo enviado satisfactoriamente";
+$lang['emailSentFail']		    = "Ocurrió un error al enviar tu correo, intenta nuevamente.";
+
+$lang['failSearch']				= 'No se han encontrado resultados con tus parámetros de búsqueda';
+
+//CAMBIO DE DATOS
+$lang['contrasenaActualFail']   = "Tu contraseña actual es incorrecta.";
+$lang['contrasena2Fail']   = "Las contraseñas no son iguales.";
+
+// UPDATE, DELETE, AND CREATE
+$lang['updateOk']			    = "Información actualizada satisfactoriamente.";
+$lang['insertOk']			    = "Información agregada satisfactoriamente.";
+$lang['deleteOk']			    = "Borrado satisfactorio.";
+$lang['updateFail']			    = "Ocurrió un error al actualizar tu información, intenta nuevamente.";
+$lang['insertFail']			    = "Ocurrió un error al agregar tu información, intenta nuevamente.";
+$lang['deleteFail']			    = "Ocurrió un error al realizar el borrado, intenta nuevamente.";
+$lang['imageSizeFail']		    = "La imagen que intenta subir es más grande que el tamaño permitido";
+$lang['insertOKm']		    = "Su usuario ha sido creado exitosamente, se han enviado las credenciales al correo antes proporcionado.";
+
+// DATA VALIDATION
+$lang['bannedOK']               = "Usuario Baneado Satisfactoriamente";
+$lang['unblockedUser']          = "Usuario Desbloqueado Satisfactoriamente";
+$lang['infoIncorrect']		    = "Información Incorrecta.";
+$lang['inactiveUser']		    = "Debes activar tu usuario antes de iniciar sesión.";
+$lang['bannedUser']			    = "Tu usario ha sido banneado, contacta al administrador para más información.";
+$lang['canceled']			    = "Tu cuenta ha sido cancelada";
+$lang['passNoIguales']		    = "Las contraseñas no son iguales.";
+$lang['passReseted']		    = "Tu nueva contraseña fue enviada a tu correo electrónico.";
+$lang['rolNoPuedeDel']		    = "El Rol no se puede eliminar, existen usuarios asignados a este.";
+$lang['enviado']		    = "Tu mensaje ha sido enviado.";
+$lang['noEnviado']		    = "Tu mensaje no fue enviado.";
+
+// FILE UPLOAD
+$lang['fileInvalidType'] 	    = "Tipo de archivo inválido.";
+$lang['fileUploadingFail'] 	    = "Ocurrio un error al subir tu archivo.";
+
+// FORM VALIDATION
+$lang['inputRequired']		    = "El campo \'%s\' es obligatorio";
+$lang['inputMinLength']		    = "El campo \'%s\' no puede contener menos de %d caracteres";
+$lang['inputMaxLength']		    = "El campo \'%s\' no puede contener más de %d caracteres";
+$lang['inputBothEqual']		    = "Los campos \'%s\' y \'%d\' tienen que ser iguales";
+$lang['inputEitherSel']		    = "Alguno de los campos \'%s\', \'%d\' debe de estar lleno";
+$lang['inputBadSyntax']		    = "El campo \'%s\' contiene errores de sintaxis o está mal escrito";
+
+$lang['admin404']			    = "El contenido que está buscando no existe o ha sido borrado.";
+$lang['error404']		        = "El contenido que está buscando no existe o ha sido borrado.";
+
+// Security issues
+
+$lang['invalidToken']		    = "Se ha cancelado tu solicitud por cuestión de seguridad, recarga la página y vuelve a intentarlo.";
+?>
